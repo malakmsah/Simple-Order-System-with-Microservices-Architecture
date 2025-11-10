9ff8386d52961968618c279174a31e45809bbdf7
Malak <malak.abuhammad@gmail.com>
2025-11-10 16:04:38 +0300

    orders service

--- product_service/products/models.py
+++ product_service/products/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=255, unique=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
